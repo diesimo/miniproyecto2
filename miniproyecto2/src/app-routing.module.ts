07f47b50f98ef684c61414ad689ba258e1af7ec8
Diego Simoes <dsimoes@correo.unimet.edu.ve>
2021-03-05 13:03:32 -0400

    Login and Register

--- miniproyecto2/src/app-routing.module.ts
+++ miniproyecto2/src/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {LoginComponent} from './components/login/login.component';
+import {RegistrarseComponent} from './components/registrarse/registrarse.component'
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'registrarse', component:RegistrarseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- miniproyecto2/src/app.module.ts
+++ miniproyecto2/src/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule,
